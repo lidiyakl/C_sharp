0951065ff930ce70c40bbd992430bb96d86f23ba
«Lidiya» <lidiyakl0407@gmail.com>
2022-11-26 22:06:24 +0700

    Task 1_1

---
+++ 1_lesson/1_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Write a number1: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write a number2: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if(num2 == num1*num1)
+{
+    Console.WriteLine("да");
+}
+
+else
+Console.WriteLine("нет");
