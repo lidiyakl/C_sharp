a7d7501fde2ef14d3e2f6374635b3e0006c3023e
«Lidiya» <lidiyakl0407@gmail.com>
2022-11-26 22:55:27 +0700

    Task 1_4

---
+++ 1_lesson/1_4/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число");
+var y = Console.ReadLine();
+int N = y == null ? 0 : int.Parse(y);
+Console.WriteLine($"{Math.Abs(N)}");
