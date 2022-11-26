1e895ef6307b1614ce9101b9b489dfdbd1ed3f46
«Lidiya» <lidiyakl0407@gmail.com>
2022-11-26 22:19:10 +0700

    Task 1_2

---
+++ 1_lesson/1_2/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int y = int.Parse(Console.ReadLine());
+if (y == 1)
+{
+   Console.WriteLine("Понедельник"); 
+}
+else if (y == 2)
+{
+   Console.WriteLine("Вторник"); 
+}
+else if (y == 3)
+{
+   Console.WriteLine("Среда"); 
+}
+else if (y == 4)
+{
+   Console.WriteLine("Четверг"); 
+}
+else if (y == 5)
+{
+   Console.WriteLine("Пятница"); 
+}
+else if (y == 6)
+{
+   Console.WriteLine("Суббота"); 
+}
+else if (y == 7)
+{
+   Console.WriteLine("Воскресенье"); 
+}
+else
+{
+   Console.WriteLine("error"); 
+}
