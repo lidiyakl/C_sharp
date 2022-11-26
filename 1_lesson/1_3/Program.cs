c6949178767c93d373f18c4a5440cbb34adf7e47
«Lidiya» <lidiyakl0407@gmail.com>
2022-11-26 22:38:49 +0700

    Task 1_3

---
+++ 1_lesson/1_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+string x = Console.ReadLine();
+int N = int.Parse(x);
+int NegN = - N;
+
+while(NegN <= N)
+{
+    Console.Write($"{NegN} ");
+    NegN +=1;
+}
