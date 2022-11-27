fa5cf055b3683b0d60b22c16c8f98da49201d761
«Lidiya» <lidiyakl0407@gmail.com>
2022-11-27 22:20:18 +0700

    HM task 2

---
+++ 1_lesson/Homework/1-2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число1: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число2: ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число3: ");
+int num3 = int.Parse(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max)
+{
+    max = num2;
+}
+else if (num3 > max)
+{
+    max = num3;
+}
+
+Console.WriteLine(max);
+
+
