d1b282fd2395aec9e1f9da1cb5260fdf8ce57515
«Lidiya» <lidiyakl0407@gmail.com>
2022-11-29 22:53:57 +0700

    Task 2_3

--- 2_lesson/2_3/Program.cs
+++ 2_lesson/2_3/Program.cs
@@ -1,4 +1,15 @@
 ﻿// Напищите программу, которая принимает на вход число и проверяет, кратно ли оно
 //одновременно 7 и 23.
 
-Console.WriteLine("Hello, World!");
+string Crat(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0)
+        return "кратно";
+    else
+        return "некратно";
+}
+
+Console.WriteLine("Введите число");
+int number = int.Parse (Console.ReadLine());
+
+Console.WriteLine(Crat(number));
