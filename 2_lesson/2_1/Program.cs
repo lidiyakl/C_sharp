db925f5b13c4d16e8fbee44d23ac7b6eeb3867d3
«Lidiya» <lidiyakl0407@gmail.com>
2022-11-29 22:06:28 +0700

    Task 2_1

---
+++ 2_lesson/2_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводт случайное трехзначное число
+// и на выходе показывает последнюю цифру этого числа
+
+int Num(int num)
+{
+    Console.WriteLine(num);
+
+    return num % 10; 
+}
+
+Console.WriteLine(Num (new Random().Next(100, 1000)));
