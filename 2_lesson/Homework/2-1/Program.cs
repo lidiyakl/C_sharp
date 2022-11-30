cdd4f499283f1240e772280dc510f555ad152ade
«Lidiya» <lidiyakl0407@gmail.com>
2022-11-30 23:04:46 +0700

    HM Task 2-1

---
+++ 2_lesson/Homework/2-1/Program.cs
@@ -0,0 +1,13 @@
+﻿string Num(int num)
+{
+    if ((num > 99 && num < 1000) || (num > -1000 && num < -99))
+        
+        return $"{num/10 % 10}";
+    
+    return "error";
+}
+
+Console.WriteLine("Введите число");
+int number = int.Parse (Console.ReadLine());
+
+Console.WriteLine(Num(number));
---
+++ 2_lesson/Homework/2-2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int num = int.Parse (Console.ReadLine());
+
+if ((num > 99 && num < 1000) || (num > -1000 && num < -99))
+{
+        num = num / 10;
+        Console.WriteLine (num % 10);
+}
+        
+else
+{
+    Console.WriteLine ("error");
+}
+        
