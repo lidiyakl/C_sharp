2fbe87f5bf63ea79590c7dd6919897b0dccca35f
«Lidiya» <lidiyakl0407@gmail.com>
2022-11-30 23:08:57 +0700

    HM Task 2-2

--- 2_lesson/Homework/2-2/Program.cs
+++ 2_lesson/Homework/2-2/Program.cs
@@ -1,15 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите число");
-int num = int.Parse (Console.ReadLine());
+﻿int Number(int num)
+{
+    Console.WriteLine(num);
 
-if ((num > 99 && num < 1000) || (num > -1000 && num < -99))
-{
-        num = num / 10;
-        Console.WriteLine (num % 10);
+    return num / 10;
 }
-        
-else
-{
-    Console.WriteLine ("error");
-}
+
+Console.WriteLine(Number (new Random().Next(100, 1000)));
         
