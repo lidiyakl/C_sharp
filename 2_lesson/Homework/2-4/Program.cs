557fe90294cc474152b84649d6b14bd5e733fa98
«Lidiya» <lidiyakl0407@gmail.com>
2022-12-01 23:23:27 +0700

    HM Task 2-4

---
+++ 2_lesson/Homework/2-4/Program.cs
@@ -0,0 +1,12 @@
+﻿string Week(int num)
+{
+    if (num == 6 || num == 7)
+        return "yes";
+    else
+        return "no";
+}
+
+Console.WriteLine("Введите число");
+int number = int.Parse (Console.ReadLine());
+
+Console.WriteLine(Week(number));
