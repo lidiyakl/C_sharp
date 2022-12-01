1d035f6bdc0ec61938db8b2db1a9329df36910d1
«Lidiya» <lidiyakl0407@gmail.com>
2022-12-01 23:15:50 +0700

    HM Task 2-3

--- 2_lesson/Homework/2-3/Program.cs
+++ 2_lesson/Homework/2-3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Num(int num)
+{
+    if (num < 100)
+    {
+        Console.WriteLine("третьей цифры нет");
+        return;
+    }
+
+    while (num > 999) num /= 10;
+    Console.WriteLine(num % 10);
+}
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Num(number));
