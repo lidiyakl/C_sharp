03ee170e910e201f0abfcb7fdf441ef470c5662a
«Lidiya» <lidiyakl0407@gmail.com>
2022-12-02 23:06:04 +0700

    Task 3_1

---
+++ 3_lesson/3_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу. которая по заданному номеру четверти показывает
+//диапазон возможных координат точек в этой четверти (х и y)
+
+void Quarters(string a)
+{
+    if (a == "I")
+       Console.WriteLine("x > 0 and y > 0");
+    else if (a == "II")
+       Console.WriteLine("x > 0 and y < 0");
+    else if (a == "III")
+       Console.WriteLine("x < 0 and y < 0");
+    else if (a == "I")
+       Console.WriteLine("x > 0 and y < 0");
+    else
+       Console.WriteLine("error");      
+}
+
+Quarters(Console.ReadLine());
