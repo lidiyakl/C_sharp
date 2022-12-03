c848f3181be27bd97bbaba92363bd9a33338c0e6
«Lidiya» <lidiyakl0407@gmail.com>
2022-12-03 22:38:45 +0700

    Task 3_3

---
+++ 3_lesson/3_3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу
+//квадратов чмсел от 1 до N.
+
+void Chet(int N)
+{
+int i = 0;
+double pere;
+while(i < N)
+{
+        i += 1;
+        if(i == N)
+        {
+        pere = Math.Pow(i,2);
+        Console.Write($"{pere}");
+        } 
+        else
+        {
+
+        pere = Math.Pow(i,2);
+        Console.Write($"{pere}, ");
+        }
+}
+}
+Console.WriteLine("Введите число");
+int chislo = int.Parse(Console.ReadLine());
+Chet(chislo);
