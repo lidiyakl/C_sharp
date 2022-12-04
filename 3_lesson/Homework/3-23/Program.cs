9f95335217e4323bd66c86542c18ac33d53ffc07
«Lidiya» <lidiyakl0407@gmail.com>
2022-12-04 21:01:08 +0700

    HW Task 3-23

---
+++ 3_lesson/Homework/3-23/Program.cs
@@ -0,0 +1,23 @@
+﻿void Chet(int N)
+{
+int i = 0;
+double pere;
+while(i < N)
+{
+        i += 1;
+        if(i == N)
+        {
+        pere = Math.Pow(i,3);
+        Console.Write($"{pere}");
+        } 
+        else
+        {
+
+        pere = Math.Pow(i,3);
+        Console.Write($"{pere}, ");
+        }
+}
+}
+Console.WriteLine("Введите число");
+int chislo = int.Parse(Console.ReadLine());
+Chet(chislo);
