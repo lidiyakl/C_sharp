7a14ad11f2536f506e363f1c3a7d80e06df9c374
«Lidiya» <lidiyakl0407@gmail.com>
2022-12-04 21:17:38 +0700

    HW Task 3-19

---
+++ 3_lesson/Homework/3-19/Program.cs
@@ -0,0 +1,22 @@
+﻿void Pal(int N)
+{
+    if(N > 9999 && N < 99999)
+    {
+        if(N % 10 == N / 10000 && N / 10 % 10 == N / 1000 % 10)
+        {
+            Console.WriteLine("да");
+        }
+        else
+        {
+          Console.WriteLine("нет");
+        }
+    }
+    else
+    {
+        Console.WriteLine("error");
+    }
+}
+
+Console.WriteLine("Введите число");
+int chislo = int.Parse(Console.ReadLine());
+Pal(chislo);
