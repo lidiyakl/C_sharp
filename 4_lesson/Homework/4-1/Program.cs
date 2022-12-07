c4e11e95b77446fc12346c45b791f34e0fde4d84
«Lidiya» <lidiyakl0407@gmail.com>
2022-12-07 22:23:18 +0700

    HW Task 4-1

---
+++ 4_lesson/Homework/4-1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите цикл, который принимает на вход два числа (А и В)
+// И возводит число А в натуральную степень В.
+
+int Step(int a, int b)
+{
+    int avb=1;
+    for(int i=0; i<b; i++)
+    {
+        avb = avb*a;
+    }
+
+    return avb;
+}
+
+Console.WriteLine("Введите число A");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int B = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Step(A,B));
