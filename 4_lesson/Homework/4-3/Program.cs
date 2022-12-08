54c0bbc6493d4658ec6d3532e75be2952a6f84b6
«Lidiya» <lidiyakl0407@gmail.com>
2022-12-08 20:53:47 +0700

    HW Task 4-3

---
+++ 4_lesson/Homework/4-3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая задает массив из 8 элементов и выводит их на экран.
+
+int[] Massiv(int size)
+{
+    int[] collection = new int[size];
+    int index = 0;
+    while (index < size)
+    {
+        collection[index] = new Random().Next(1, 100);
+        index++;
+    }
+    return collection;
+}
+
+void PrintArray(int[] collection)
+{
+    int ind = 0;
+    int count = collection.Length;
+    while (ind < count)
+    {
+        Console.Write($"{collection[ind]} ");
+        ind++;
+    }
+    Console.WriteLine();
+}
+
+int[] collection2 = Massiv(int.Parse(Console.ReadLine()));
+PrintArray(collection2);
