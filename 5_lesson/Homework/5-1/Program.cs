05a9cf8eac39d3a4e47d3c068f3b6fc4409c382b
«Lidiya» <lidiyakl0407@gmail.com>
2022-12-11 22:07:17 +0700

    HW Task 5-1

---
+++ 5_lesson/Homework/5-1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте массив, заполненный случайными положительными трехзначными числами.
+//Напишите программу, которая покажет количество четных чисел в массиве.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(100, 1000);
+    return arr;
+}
+
+int Count(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+int[] arr_1 = MassNums(int.Parse(Console.ReadLine()));
+Print(arr_1);
+Console.WriteLine(Count(arr_1));
