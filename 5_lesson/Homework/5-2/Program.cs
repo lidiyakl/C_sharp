12cbb31c558e6a6e212bfb6948272abb60ac7e3b
«Lidiya» <lidiyakl0407@gmail.com>
2022-12-11 22:19:07 +0700

    HW Task 5-2

---
+++ 5_lesson/Homework/5-2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+//Найдите сумму элементов, стоящих на нечетных позициях.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+
+int Sum(int[] arr)
+{
+    int sum = 0;
+
+    for (int i = 0; i < arr.Length; i += 2)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int[] arr_1 = MassNums(int.Parse(Console.ReadLine()),
+                       int.Parse(Console.ReadLine()),
+                       int.Parse(Console.ReadLine()));
+Print(arr_1);
+Console.WriteLine(Sum(arr_1));
